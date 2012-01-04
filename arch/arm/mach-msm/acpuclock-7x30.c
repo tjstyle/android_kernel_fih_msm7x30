54fc2d6bec23a8a99e93d68113aa2c8c37971ab4
Teguh Sobirin <teguh@sobir.in>
2012-01-05 05:55:48 +0700

    cpu: implement overclocking and export scaling_available_frequencies attr in /sys.

--- arch/arm/mach-msm/acpuclock-7x30.c
+++ arch/arm/mach-msm/acpuclock-7x30.c
@@ -92,10 +92,21 @@ static struct clock_state drv_state = { 0 };
 static struct clkctl_acpu_speed *backup_s;
 
 static struct pll pll2_tbl[] = {
+	{  25, 0, 1, 0 }, /* 480 MHz */
+	{  28, 1, 3, 0 }, /* 544 MHz */
+	{  31, 1, 4, 0 }, /* 600 MHz */
 	{  42, 0, 1, 0 }, /*  806 MHz */
 	{  53, 1, 3, 0 }, /* 1024 MHz */
 	{ 125, 0, 1, 1 }, /* 1200 MHz */
 	{  73, 0, 1, 0 }, /* 1401 MHz */
+	{  79, 0, 1, 0 }, /* 1516 MHz */
+	{  84, 0, 1, 0 }, /* 1612 MHz */
+	{  94, 0, 1, 0 }, /* 1804 MHz */
+	{  100, 0, 1, 0 }, /* 1920 MHz */
+	{  104, 0, 1, 0 }, /* 1997 MHz */
+	{  108, 0, 1, 0 }, /* 2074 MHz */
+	{  110, 0, 1, 0 }, /* 2112 MHz */
+
 };
 
 /* Use negative numbers for sources that can't be enabled/disabled */
@@ -115,15 +126,25 @@ static struct clkctl_acpu_speed acpu_freq_tbl[] = {
 	{ 0, MAX_AXI_KHZ, SRC_AXI, 1, 0, 61440000, 900, VDD_RAW(900) },
 	{ 1, 245760, PLL_3,    5, 2,  61440000,  900, VDD_RAW(900) },
 	{ 1, 368640, PLL_3,    5, 1,  122800000, 900, VDD_RAW(900) },
+	{ 1, 480000, PLL_2,    3, 0,  122800000, 900, VDD_RAW(900), &pll2_tbl[0] },
+	{ 1, 544000, PLL_2,    3, 0,  122800000, 900, VDD_RAW(900), &pll2_tbl[1] },
+        { 1, 600000, PLL_2,    3, 0,  122800000, 925, VDD_RAW(925), &pll2_tbl[2] },   
 	/* AXI has MSMC1 implications. See above. */
 	{ 1, 768000, PLL_1,    2, 0,  153600000, 1050, VDD_RAW(1050) },
 	/*
 	 * AXI has MSMC1 implications. See above.
 	 */
-	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[0]},
-	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[1]},
-	{ 1, 1200000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[2]},
-	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[3]},
+	{ 1, 806400,  PLL_2, 3, 0, UINT_MAX, 1100, VDD_RAW(1100), &pll2_tbl[3]},
+	{ 1, 1024000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[4]},
+	{ 1, 1200000, PLL_2, 3, 0, UINT_MAX, 1200, VDD_RAW(1200), &pll2_tbl[5]},
+	{ 1, 1401600, PLL_2, 3, 0, UINT_MAX, 1250, VDD_RAW(1250), &pll2_tbl[6]},
+	{ 1, 1516800, PLL_2, 3, 0, UINT_MAX, 1300, VDD_RAW(1300), &pll2_tbl[7]},
+	{ 1, 1612800, PLL_2, 3, 0, UINT_MAX, 1350, VDD_RAW(1350), &pll2_tbl[8]},
+	{ 1, 1804800, PLL_2, 3, 0, UINT_MAX, 1350, VDD_RAW(1400), &pll2_tbl[9]},
+	{ 1, 1920000, PLL_2, 3, 0, UINT_MAX, 1400, VDD_RAW(1450), &pll2_tbl[10]},
+	{ 1, 1996800, PLL_2, 3, 0, UINT_MAX, 1400, VDD_RAW(1450), &pll2_tbl[11]},
+	{ 1, 2073600, PLL_2, 3, 0, UINT_MAX, 1400, VDD_RAW(1450), &pll2_tbl[12]},
+	{ 1, 2112000, PLL_2, 3, 0, UINT_MAX, 1400, VDD_RAW(1500), &pll2_tbl[13]},
 	{ 0 }
 };
 
@@ -453,26 +474,18 @@ static inline void setup_cpufreq_table(void) { }
 #endif
 
 /*
- * Truncate the frequency table at the current PLL2 rate and determine the
+ * Don't truncate the frequency table at the current PLL2 rate but do determine the
  * backup PLL to use when scaling PLL2.
  */
 void __init pll2_fixup(void)
 {
 	struct clkctl_acpu_speed *speed = acpu_freq_tbl;
-	u8 pll2_l = readl(PLL2_L_VAL_ADDR) & 0xFF;
 
 	for ( ; speed->acpu_clk_khz; speed++) {
 		if (speed->src != PLL_2)
 			backup_s = speed;
-		if (speed->pll_rate && speed->pll_rate->l == pll2_l) {
-			speed++;
-			speed->acpu_clk_khz = 0;
-			return;
-		}
 	}
-
-	pr_err("Unknown PLL2 lval %d\n", pll2_l);
-	BUG();
+	return;
 }
 
 #define RPM_BYPASS_MASK	(1 << 3)
